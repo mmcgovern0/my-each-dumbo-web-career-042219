b93c9122d77e5cbaa75e6d02603384d09419a654
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-12 02:44:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + i
+  end
+  collection
+end
+
+my_each(collection) do |i|
+  puts i
 end
